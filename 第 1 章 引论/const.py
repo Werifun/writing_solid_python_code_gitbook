a152dbf6b79355cb478830ac323bc7ecd54a080d
zhangwei <weizhang284@creditease.cn>
2019-08-08 11:37:33 +0800

    add const.py and constant.py

---
+++ 第 1 章 引论/const.py
@@ -0,0 +1,35 @@
+#coding:utf-8
+
+class _const:
+    class ConstError(TypeError):pass  # TyperError 指对类型无效的操作
+    class ConstCaseError(ConstError):pass
+
+    def __setattr__(self, key, value):  # 在对类的属性赋值的时候会自动调用
+        if key in self.__dict__:  # object.__dict__以dict的方式存储object所有可读属性
+            raise self.ConstError("Can't change const.%s" %key)
+        if not key.isupper():
+            raise self.ConstCaseError("const name %s is not all uppercase" %key)
+
+        self.__dict__[key] = value
+
+    def __getitem__(self,key):  # 了解一下__getitem__的功能，并且使用getitem可以像字典一样获取常量
+        if key in self.__dict__:
+            return self.__dict__[key]
+        else:
+            raise self.ConstError("Can't return const.%s, No Existsing Key!"%key)
+
+
+
+
+import sys
+sys.modules[__name__] = _const()
+# sys.modules保存有当前Python环境中已经导入的模块记录，这是一个全局字典，当Python启动后就加载在内存中。
+# dict的key为文件名，value为模块对象。
+# __name__就是标识模块的名字的一个系统变量。
+# 如果模块被导入，__name__就是模块的名字
+# 如果模块被直接执行，__name__的值是'__name__'
+
+# sys.modules['const'] = _const()
+# 即，让_const类作为模块的入口点，引入const.py等价于声明了一个_const类的实例。
+
+
---
+++ 第 1 章 引论/constant.py
@@ -0,0 +1,15 @@
+#coding:utf-8
+
+import const
+
+# const.PI = 3.1415926
+# print(const['PI'])  # __getitem__的功能
+# print(const.PI)
+
+const.TEXTLINE = 'textline'
+const.TEXTBOX = 'textbox' #
+const.TEXT_SIZE = 'size'  # 字体大小
+const.BBOX = 'bbox'  #
+const.FONT = 'font'  # 字体
+const.FOOTNOTE = 'footnote'  # 脚注
+const.TEXT = 'text'  # 文本
